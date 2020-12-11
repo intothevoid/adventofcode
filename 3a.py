b15b54a1fb3493ccf0e74e555f3f0e80431bf9b0
Karan Kadam <karank@outlook.com>
2020-12-11 17:23:00 +1030

    soln for puzzle 3a

---
+++ 3a.py
@@ -0,0 +1,66 @@
+import copy
+
+main_list = []
+col_width = 0
+row_width = 0
+
+def process_input():
+    global col_width, row_width
+    print("Processing input file...")
+
+    with open("3.txt") as input_file:
+        line = input_file.readline()
+        inner_list = []
+        
+        while line:
+            inner_list.clear()
+
+            for char in line:
+                inner_list.append(char)
+            
+            # record col_width, row_width
+            col_width = len(inner_list)
+            row_width = row_width + 1
+            
+            # add to main list
+            main_list.append(copy.copy(inner_list))
+            
+            # read next line of input
+            line = input_file.readline()
+
+def traverse_list_count_trees():
+    colidx = 0
+    rowidx = 0
+    char = '' # can be tree (#) or empty (.)
+    tree_cnt = 0
+
+    for idx in range(len(main_list)):
+        colidx = colidx + 3 
+        rowidx = rowidx + 1
+
+        # maxed our rows / end of file
+        if rowidx >= row_width:
+            break
+        
+        # rotate
+        if colidx >= col_width:
+            colidx = abs(col_width - colidx)
+
+        # get char to check
+        char = main_list[rowidx][colidx]
+        
+        # if char is tree, increment count
+        if char == "#":
+            tree_cnt = tree_cnt + 1
+        
+    
+    return tree_cnt
+
+if __name__ == "__main__":
+    # read input file
+    process_input()
+
+    # traverse as per 3 right 1 down and count trees (#)
+    tree_cnt = traverse_list_count_trees()
+
+    print(f"Found {tree_cnt} trees along the ride!")
